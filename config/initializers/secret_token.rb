0ec9fa986028155c678dbe6e4f1d84ca4b4f3923
Hephzibah <hephzibah.1989@gmail.com>
2012-06-30 23:53:04 +0530

    testing

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Happybdayniti::Application.config.secret_token = 'aae7cfdc5725667a1271e94a141da90d13b0fd4677619217ce3e519e0fa1dfef7c513e8bb6de0ccee781a377ef6e0f4251ead669a4d3f3685650a4f5d0839106'
